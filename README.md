ff793fbe1448fb90ca69dccb1c525b39ce64215c
mmslotin <balu54@yandex.ru>
2022-11-23 17:29:38 +0300

    New homework was added

---
+++ README.md
@@ -0,0 +1,13 @@
+# QA_Guru_HomeWork_15_17
+Домашнее задание 17
+
+To run web tests locally:
+```shell
+gradle clean test -DtestType=local
+```
+
+
+To run web tests remotely:
+```shell
+gradle clean test -DtestType=remote
+```
---
+++ src/test/java/WebConfigurationTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import config.WebConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebConfigurationTest {
+
+    @BeforeAll
+    static void setup() {
+        WebConfig config = ConfigFactory.create(WebConfig.class);
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browser = config.browser();
+        Configuration.browserVersion = config.browserVersion();
+        Configuration.remote = config.remoteDriverUrl();
+    }
+
+    @Test
+    void checkPageWithExampleJUnitCode() {
+        //Открыть страницу Selenide в Github
+        open("/selenide/selenide");
+
+
+    }
+}
